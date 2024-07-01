e95080713ed5ba57dc2845af6aa547052acf52ad
Md Moinuddin <noobycox@proton.me>
2024-07-01 21:37:35 +0530

    added banner for under-the-hood project

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+
+
+const Banner = () => {
+    return (
+            <div className="color-slate-700 stickey top-0 h-[3rem] m-auto bg-slate-200 flex justify-center items-center">
+                <h1 className="text-center">Project <a href="https://under-the-hood.vercel.app/" target="_blank" className="font-semibold">Under-the-hood</a> is brewing🍺</h1>
+            </div>
+
+    );
+}
+
+export default Banner;
