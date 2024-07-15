dddb61fff49798ae466d20375b2acf2273fa9ece
Md Moinuddin <noobycox@proton.me>
2024-07-16 03:21:46 +0530

    improved talk and projects route cards

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,34 +1,59 @@
-import { BsArrowUpRight } from "react-icons/bs";
 import { useState } from "react";
+import { MdArrowForwardIos } from "react-icons/md";
 
 const ProjectCard = (props) => {
     const [readActive, setReadActive] = useState(true);
-    const descContentLenfth = 250
-    return (
-        <div className="mb-[5rem]">
-            <img src={"/project/" + props.image} className="min-w-[10rem] max-h-[25rem] mb-2 object-contain overflow-hidden" alt="Project image" />
-            <div>
-                <h1 className="text-slate-900 font-base mb-[2rem] text-2xl leading-[2.5rem] underline underline-offset-4 decoration-slate-200">{props.title}</h1>
-                <p className="text-slate-700 text-xl leading-[2.5rem] inline ">{readActive ? (props.description.length > descContentLenfth? props.description.slice(0,250) : props.description ) : props.description}</p>
-                {
-                    readActive ? (props.description.length > descContentLenfth && <button className={readActive? "text-md ml-2 font-semibold": "hidden"} onClick={() => setReadActive((prevState)=> !prevState)}>read more...</button>) : (props.description.length > descContentLenfth && <button className={readActive? "hidden": "text-md ml-2 font-semibold"} onClick={() => setReadActive((prevState)=> !prevState)}>read less...</button>)
-                }
-                <br/>
-                {
-                    props.codebaseUrl && (<button className="bg-slate-100 rounded-full p-2 m-1 hover:bg-black hover:text-white">
-                    <a href={props.codebaseUrl} target="_blank">Codebase <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-                
-                {
-                    props.projectUrl && (<button className="bg-slate-100 rounded-full p-2 m-1  hover:bg-black hover:text-white">
-                    <a href={props.projectUrl} target="_blank">Project <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
+    const descContentLength = 100;
+    const truncatedDescription = props.description.slice(0, descContentLength);
 
-                {
-                    props.videoUrl && (<button className="bg-slate-100 rounded-full p-2 m-1 hover:bg-black hover:text-white">
-                    <a href={props.videoUrl} target="_blank">Video <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-                
+    return (
+        <div className="mb-12 p-4 bg-white shadow-2xl rounded-lg max-w-sm mx-auto transition-transform transform hover:scale-105">
+            <img 
+                src={"/project/" + props.image} 
+                className="w-full h-48 object-cover rounded-t-lg mb-4" 
+                alt="Project image" 
+            />
+            <div>
+                <h1 className="text-slate-900 font-semibold mb-6 text-2xl leading-snug underline underline-offset-4 decoration-slate-200">
+                    {props.title}
+                </h1>
+                <p className="text-slate-700 text-xl leading-snug mb-4">
+                    {readActive ? truncatedDescription : props.description}
+                    {props.description.length > descContentLength && (
+                        <button 
+                            className="text-md ml-2 font-semibold text-blue-600 hover:underline" 
+                            onClick={() => setReadActive(!readActive)}
+                        >
+                            {readActive ? "read more..." : "read less..."}
+                        </button>
+                    )}
+                </p>
+                <div className="space-x-2">
+                    {props.codebaseUrl && (
+                        <a 
+                            href={props.codebaseUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Codebase <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                    {props.projectUrl && (
+                        <a 
+                            href={props.projectUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Project <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                    {props.videoUrl && (
+                        <a 
+                            href={props.videoUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Video <MdArrowForwardIost className="ml-1" />
+                        </a>
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/components/talk/TalkCard.jsx
+++ src/components/talk/TalkCard.jsx
@@ -1,35 +1,54 @@
-import { BsArrowUpRight } from "react-icons/bs";
-
+import { MdArrowForwardIos } from "react-icons/md";
 const TalkCard = (props) => {
     return (
-        <div className="mb-[5rem]">
-            <img src={"/talks/" + props.image} className="min-w-[10rem] max-h-[25rem] mb-2 object-contain overflow-hidden" alt="Meetup image" />
+        <div className="mb-12 p-4 w-[30rem] bg-white shadow-2xl rounded-lg max-w-sm mx-auto transition-transform transform hover:scale-105">
+            <img 
+                src={"/talks/" + props.image} 
+                className="w-full h-48 object-cover rounded-t-lg mb-4" 
+                alt="Meetup image" 
+            />
             <div>
-                <h1 className="text-slate-900 font-base mb-[2rem] text-2xl leading-[2.5rem] underline underline-offset-4 decoration-slate-200">{props.title}</h1>
+                <h1 className="text-slate-900 font-semibold mb-6 text-2xl leading-snug underline underline-offset-4 decoration-slate-200">
+                    {props.title}
+                </h1>
 
-                {
-                    props.blogUrl && (<button className="bg-slate-100 rounded-full p-2 m-1 hover:bg-black hover:text-white">
-                    <a href={props.blogUrl} target="_blank">Blog <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-                
-                {
-                    props.videoUrl && (<button className="bg-slate-100 rounded-full p-2 m-1  hover:bg-black hover:text-white">
-                    <a href={props.videoUrl} target="_blank">Video <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-
-                {
-                    props.postUrl && (<button className="bg-slate-100 rounded-full p-2 m-1 hover:bg-black hover:text-white">
-                    <a href={props.postUrl} target="_blank">Post <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-                
-                {
-                    props.pptUrl && (<button className="bg-slate-100 rounded-full p-2 m-1 hover:bg-black hover:text-white">
-                    <a href={props.pptUrl} target="_blank">Slides <span><BsArrowUpRight className="inline mb-2"/></span></a></button>)
-                }
-                
+                <div className="space-x-2">
+                    {props.blogUrl && (
+                        <a 
+                            href={props.blogUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Blog <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                    {props.videoUrl && (
+                        <a 
+                            href={props.videoUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Video <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                    {props.postUrl && (
+                        <a 
+                            href={props.postUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Post <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                    {props.pptUrl && (
+                        <a 
+                            href={props.pptUrl} 
+                            target="_blank" 
+                            className="hover:bg-[#F5F5F5] rounded-md font-bold px-4 py-2 m-1 inline-flex items-center">
+                            Slides <MdArrowForwardIos className="ml-1" />
+                        </a>
+                    )}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default TalkCard;
