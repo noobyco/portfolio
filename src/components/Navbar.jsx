64fb5413679557a66539c13adb59ffef4e989f81
Md Moinuddin <noobycox@proton.me>
2024-07-20 19:24:14 +0530

    menu transition enabled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 'use client'
-import { useState, useEffect} from "react";
-import { FaHamburger } from "react-icons/fa";
+import { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
-
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 const Navbar = (props) => {
     const [btnState, setBtnState] = useState(false);
 
@@ -23,17 +22,25 @@ const Navbar = (props) => {
 
             <div className="z-[50] h-[2rem] w-fit sm:hidden mr-[3rem]">
                 <button onClick={ ()=> setBtnState((prevStateBtn) => !prevStateBtn)} className="cursor-pointer">
-                    { btnState ? (<RxCross1 size={30} color={"black"} />) : (<FaHamburger size={30}/>)}
+                    { btnState ? (<RxCross1 size={30} color={"black"} />) : (<HiOutlineMenuAlt3 size={30}/>)}
                 </button>
             </div>
-            {btnState && 
+            {btnState ? 
             
-            <div className="z-[30] absolute top-0 left-0 h-screen w-screen flex flex-col justify-center gap-16 items-center text-4xl font-semibold bg-gradient-to-bl from-blue-800 to-black">
+            <div className="z-[30] transition absolute top-0 left-0 h-screen w-screen flex flex-col justify-center gap-16 items-center text-4xl font-semibold bg-gradient-to-bl from-blue-800 to-black sm:hidden">
                 <a className="tracking-wide text-[#F5F5F5]" href="/open-source">Open Source</a>
                 <a className="tracking-wide text-[#F5F5F5]" href="/projects">Projects</a>
                 <a className="tracking-wide text-[#F5F5F5]" href="https://noobyco.hashnode.dev" target="_blank">Blogs</a>
                 <a className="tracking-wide text-[#F5F5F5]" href="/talks">Talks</a>
             </div>
+            :
+            <div className="z-[30] -translate-y-[100rem] transition absolute top-0 left-0 h-screen w-screen flex flex-col justify-center gap-16 items-center text-4xl font-semibold bg-gradient-to-bl from-blue-800 to-black sm:hidden">
+            <a className="tracking-wide text-[#F5F5F5]" href="/open-source">Open Source</a>
+            <a className="tracking-wide text-[#F5F5F5]" href="/projects">Projects</a>
+            <a className="tracking-wide text-[#F5F5F5]" href="https://noobyco.hashnode.dev" target="_blank">Blogs</a>
+            <a className="tracking-wide text-[#F5F5F5]" href="/talks">Talks</a>
+        </div>
+
             }
         </nav>
     );
